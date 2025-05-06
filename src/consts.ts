0e5802863e33d0df9934dc9cae9cbb5e9b8c3d85
brumleygap <brumleygap@users.noreply.github.com>
2025-05-06 13:42:14 -0400

    Added purpose statement in About page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "TennVa UNITED!";
-export const SITE_DESCRIPTION = "Welcome to our website!";
+export const SITE_DESCRIPTION = "An alliance of Constitution defenders.";
